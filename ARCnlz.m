351c7190083835f8a8d4b2f999bbcb842a5337b7
Ben Chin <ben.chinny@gmail.com>
2023-05-02 14:30:58 -0700

    big changes to ARCnlz analysis function

--- ARCnlz.m
+++ ARCnlz.m
@@ -1,32 +1,42 @@
 function ARCnlz         
 sn=input('Subject Number? '); sn=sn+1000;
-vs=input('Visit Number? ')
+vs=input('Visit Number? ');
 ey=input('Eye? 1forRight 2forBinocular ');
 filePath = 'G:\My Drive\exp_bvams\code_repo\';
 
-% LEAVE THE FOLLOWING TWO LINES OF CODE UNCOMMENTED UNTIL I FIGURE OUT 
-% WHAT c0 IS FOR
-% load('JnJrand211130', 'TCAm')
-% c0=TCAm(sn-1000, vs-1);
-if ey(1)==2
-    load('G:\My Drive\exp_bvams\code_repo\ARC\AFCflsB.mat'); c1=2;
-elseif ey(1)==1
-    load('G:\My Drive\exp_bvams\code_repo\ARC\AFCflsR.mat'); c1=1;
-end
-fnm=AFCfls{sn-1000, vs}; load(fnm)
+cmpInfo = psyComputerInfo;
 
-%  [jsonFile,jsonPath] = uigetfile('*.json','' , 'G:\.shortcut-targets-by-id\17-MjlIMJ6eySxBl-1ikM1jyh1UQiX26y\code_repo\JnJ')
-[jsonFile,jsonPath] = uigetfile('*.json','' , 'G:\My Drive\ARchromaVideoCapture\videos\processed\centralperipheral_real') %stim conputer
-%         [jsonFile,jsonPath] = uigetfile('*.json','' , 'G:\My Drive\m-wamvideocapture\videos\processed\alwaysrecord_notcp')
+if strcmp(cmpInfo.userShortName,'benchin')
+    % THIS JUST LOADS A FILE CONTAINING FILE NAMES OF .mat FILES CONTAINING
+    % METADATA FOR EACH EXPERIMENT BLOCK
+    if ey(1)==2
+        load('/Users/benchin/Documents/ARchroma/AFCflsB.mat'); c1=2;
+    elseif ey(1)==1
+        load('/Users/benchin/Documents/ARchroma/AFCflsR.mat'); c1=1;
+    end
+    % LOADS .mat FILE CONTAINING METADATA FOR EXPERIMENT BLOCK
+    fnmTmp=AFCfls{sn-1000, vs}; load(['/Users/benchin/Documents/ARchroma/' fnmTmp(37:end)])
+    % LOADS FILE OF ACTUAL DATA
+    [jsonFile,jsonPath] = uigetfile('*.json','' , '/Users/benchin/Documents/ARchroma/');
+else
+    if ey(1)==2
+        load('G:\My Drive\exp_bvams\code_repo\ARC\AFCflsB.mat'); c1=2;
+    elseif ey(1)==1
+        load('G:\My Drive\exp_bvams\code_repo\ARC\AFCflsR.mat'); c1=1;
+    end
+    fnm=AFCfls{sn-1000, vs}; load(fnm);
+    [jsonFile,jsonPath] = uigetfile('*.json','' , 'G:\My Drive\ARchromaVideoCapture\videos\processed\centralperipheral_real'); %stim conputer
+end
 
 % dt=jsondecode(jsonFile);
 dt=jsondecode(fileread([jsonPath jsonFile]));
 
-%horizontal
+% GRABS RAW PIXEL SEPARATIONS BETWEEN AUTOREFRACTOR BARS
 x=-1.*[(cell2mat(struct2cell(dt.ext_right_mu))-cell2mat(struct2cell(dt.ext_left_mu)))];
 y=-1.*[(cell2mat(struct2cell(dt.ext_bottom_mu))-cell2mat(struct2cell(dt.ext_top_mu)))];
            
-t3=cell2mat(struct2cell(dt.time_totalsecs))
+% THIS BLOCK SORTS THE DATA BY TRIAL
+t3=cell2mat(struct2cell(dt.time_totalsecs));
 v0=[find(diff(t3)>1); length(t3)];
 i0=1;
 for k0=1:length(v0);
@@ -36,10 +46,10 @@ for k0=1:length(v0);
    i0=v0(k0)+1;
 end
 
-%i0=find(AFCv==3);
+% FINDS AUTOREFRACTOR VALUES FOR 'REFERENCE' ACCOMMODATION (SOME
+% FLEXIBILITY HERE ABOUT HOW TO DEFINE THE REFERENCE)
 i0=find(AFCp.v00(:,1)==0);
 i0 = 1;
-           
 x1=[]; y1=[];
 for k0=1:length(i0)
  x1=[x1 x0{i0(k0)}'];
@@ -47,46 +57,60 @@ for k0=1:length(i0)
 end
 x1 = x1(1:10);
 y1 = y1(1:10);
-           
-%x3=[]; y3=[]; v1=[-0.5:0.25:4]; v2=v1(AFCv); v3=[];
-clear x2 y2
-x3=[]; y3=[]; 
-% v1=AFCp.v0; 
-v2=AFCp.v00; 
-meanv2 = AFCp.meanv00;
-v3=[]; 
-% x4=ones(1,length(v2)); x5=zeros(length(v1), 4); y5=x5; 
-rgbValues = [];
 
-for k0=1:size(v2,1)
-   x2{k0}=x0{k0}-mean(x1);
-   y2{k0}=y0{k0}-mean(y1);
-   x3=[x3 x2{k0}'];
-   y3=[y3 y2{k0}'];
-   sinValuesTmp = AFCp.sinValues(k0,:);
-   tSin = 0:(1/(length(sinValuesTmp)-1)):1;
-   tSinInterp = 0:(1/(length(x2{k0})-1)):1;
-   v3 = [v3 interp1(tSin,sinValuesTmp,tSinInterp)];
-   rgbValues = [rgbValues imresize([AFCp.rgb100(k0,:)' AFCp.rgb200(k0,:)'],[3 length(tSinInterp)],'nearest')];
-%   v3=[v3 v2(k0).*ones(1,length(x2{k0}))];
-%    i1=find(v2(k0)==v1)
-%    x5(i1, x4(i1))=mean(x2{k0}./-3.59);
-%    y5(i1, x4(i1))=mean(y2{k0}./-3.33);
-%    x6(k0)=mean(x2{k0}./-3.59);
-%    y6(k0)=mean(y2{k0}./-3.33);
-%    x4(i1)=x4(i1)+1;
+% MAIN ANALYSIS
+uniqueRGBvalues = unique([AFCp.rgb100 AFCp.rgb200],'rows');
+for i = 1:size(uniqueRGBvalues,1)
+    % INDEX OF COLOR CONDITIONS
+    indCnd = find(ismember([AFCp.rgb100 AFCp.rgb200],uniqueRGBvalues(i,:),'rows'));
+    clear x2 y2 x3 y3;
+    x3=[]; y3=[]; 
+    v3=[]; 
+    rgbValues = [];
+    for k0=1:size(indCnd,1)
+       x2{k0}=x0{indCnd(k0)}-mean(x1); % MEAN CENTERING
+       y2{k0}=y0{indCnd(k0)}-mean(y1); % MEAN CENTERING
+       x3=[x3 x2{k0}'];
+       y3=[y3 y2{k0}'];
+       sinValuesTmp = AFCp.sinValues(indCnd(k0),:); % ACCOMMODATIVE DEMAND FROM EXPERIMENT
+       tSin = 0:(1/(length(sinValuesTmp)-1)):1;
+       tSinInterp = 0:(1/(length(x2{k0})-1)):1;
+       v3 = [v3 interp1(tSin,sinValuesTmp,tSinInterp)];
+       rgbValues = [rgbValues imresize([AFCp.rgb100(indCnd(k0),:)' AFCp.rgb200(indCnd(k0),:)'],[3 length(tSinInterp)],'nearest')];
+    end
+    x3=x3./-2.87; y3=y3./-2.58;
+    blinkCutoff = 3; % CUTOFF FOR REMOVING OUTLIERS
+    x3(x3>blinkCutoff | x3<-blinkCutoff) = 0;
+    y3(y3>blinkCutoff | y3<-blinkCutoff) = 0;
+    figure;
+    set(gcf,'Position',[429 518 956 420]);
+
+    subplot(1,2,1);
+    set(gca,'FontSize',15);
+    hold on;
+    plot([1:length(x3)], [x3; y3])
+    plot([1:length(x3)], [rgbValues(1,:)],'r')
+    plot([1:length(x3)], [rgbValues(2,:)],'g')
+    plot([1:length(x3)], [rgbValues(3,:)],'b')
+    xlabel('Frame'); ylabel('Power (Diopters)'); title('Autorefractor measurement'); legend('Horizontal', 'Vertical','R', 'G', 'B')
+    indMean1 = abs(rgbValues(1,:)-uniqueRGBvalues(i,1))<0.001 & ...
+               abs(rgbValues(2,:)-uniqueRGBvalues(i,2))<0.001 & ...
+               abs(rgbValues(3,:)-uniqueRGBvalues(i,3))<0.001;
+    indMean2 = abs(rgbValues(1,:)-uniqueRGBvalues(i,4))<0.001 & ...
+               abs(rgbValues(2,:)-uniqueRGBvalues(i,5))<0.001 & ...
+               abs(rgbValues(3,:)-uniqueRGBvalues(i,6))<0.001; 
+
+    subplot(1,2,2);
+    hold on;
+    bar(1,mean(x3(indMean1)),'FaceColor',uniqueRGBvalues(i,1:3));
+    bar(2,mean(x3(indMean2)),'FaceColor',uniqueRGBvalues(i,4:6));
+    bar(3,mean(y3(indMean1)),'FaceColor',uniqueRGBvalues(i,1:3));
+    bar(4,mean(y3(indMean2)),'FaceColor',uniqueRGBvalues(i,4:6));
+    axis square;
+    ylabel('Mean Accommodative Response (D)');
+    set(gca,'FontSize',15);
+    set(gca,'XTick',[1:4]);
+    set(gca,'XTickLabel',{'Horz','Horz','Vert','Vert'});
 end
 
-x3=x3./-2.87; y3=y3./-2.58;
-plot([1:length(x3)], [x3; y3])
-hold on;
-plot([1:length(x3)], [rgbValues(1,:)],'r')
-plot([1:length(x3)], [rgbValues(2,:)],'g')
-plot([1:length(x3)], [rgbValues(3,:)],'b')
-xlabel('Frame'); ylabel('Power (Diopters)'); title('Autorefractor measurement'); legend('R', 'G', 'B', 'Horizontal', 'Vertical')
-
-% %figure; plot(v1, mean(x5,2))
-% figure; errorbar(v1, mean(x5,2), std(x5,0, 2)./sqrt(4))
-% hold on;
-% errorbar(v1, mean(y5,2), std(y5,0, 2)./sqrt(4))
-% xlabel('Demand'); ylabel('Response measured'); legend('Horizontal', 'Vertical'); title(fnm) 
+end
