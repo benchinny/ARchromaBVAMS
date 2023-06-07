7579d8818ed68c503fd872784b8055ec250b359d
Ben Chin <benchin@Ascutney.local>
2023-06-07 10:18:23 -0700

    add ARCnlz_linearModel

---
+++ ARCnlz_linearModel.m
@@ -0,0 +1,183 @@
+function ARCnlz_linearModel         
+
+sn = 11; % CURRENTLY HAVE SUBJECTS 11 THROUGH 15
+bEXCLUDE = true;
+gammaFactorR = 2.4;
+
+if sn==11 % 'VISIT' NUMBERS
+   vs = [2 3 4 7];
+   excludeTrials = [];
+elseif sn==12
+   vs = [3:7];
+   excludeTrials = [87 7 3 4 5 90 91 10 6 86 88];
+elseif sn==13
+   vs = 1:4;
+   excludeTrials = [59 39 38 58 77 56 60 74];
+elseif sn==14
+   vs = 1:4;
+   excludeTrials = [];    
+elseif sn==15
+   vs = 1:4;
+   excludeTrials = [12 32 30];  
+else
+   error('ARCnlz_linearModel: unhandled subject number!');
+end
+ey = 1; % 1 for Right eye, 2 for Binocular ');
+filePath = 'G:\My Drive\exp_bvams\code_repo\';
+
+
+if strcmp(getenv('username'),'bankslab')
+   dataDirectory = [filePath 'ARC\'];
+else
+   dataDirectory = '/Users/benchin/Documents/ARchroma/'; 
+end
+% THIS JUST LOADS A FILE CONTAINING FILE NAMES OF .mat FILES CONTAINING
+% METADATA FOR EACH EXPERIMENT BLOCK
+if ey(1)==2
+    load([dataDirectory 'AFCflsB.mat']); c1=2;
+elseif ey(1)==1
+    load([dataDirectory 'AFCflsR.mat']); c1=1;
+end
+x = [];
+y = [];
+t3 = [];
+for i = 1:length(vs)
+    if strcmp(getenv('username'),'bankslab')
+        fnm=AFCfls{sn, vs(i)};
+        fnmTmp = fnm;
+        load(fnm);
+    else
+        % LOADS .mat FILE CONTAINING METADATA FOR EXPERIMENT BLOCK
+        fnmTmp=AFCfls{sn, vs(i)};         
+        load([dataDirectory fnmTmp(37:end)]);
+    end
+    if i==1
+       AFCpAll = AFCp;
+    else
+       AFCpAll = structmerge(AFCpAll,AFCp,length(AFCp.v00));
+    end
+    dateCodeAll = [];
+    % CONVERTING DATE OF .mat FILE TO JSON FILE NAME
+    dateCode = fnmTmp((end-9):end);
+    dateCodeAll = [dateCodeAll; dateCode];
+    dateCode(end) = dateCode(end)-1; % THE NEXT FEW LINES ARE FOR ADDING ROBUSTNESS
+    dateCodeAll = [dateCodeAll; dateCode];
+    dateCode(end) = dateCode(end)+2;
+    dateCodeAll = [dateCodeAll; dateCode];
+    if strcmp(getenv('username'),'bankslab')
+        jsonDirectory = 'G:\My Drive\ARchromaVideoCapture\videos\processed\centralperipheral_real\';
+    else
+        jsonDirectory = dataDirectory;
+    end    
+    jsonFile = [];
+    for j = 1:size(dateCodeAll,1) % FOR ROBUSTNESS: SOMETIMES THE FILENAMES DON'T MATCH EXACTLY
+        jsonFileStr = ['20' dateCodeAll(j,1:2) '-' dateCodeAll(j,3:4) '-' dateCodeAll(j,5:6) ' ' dateCodeAll(j,7:8) '.' dateCodeAll(j,9:10) '.json'];
+        if isfile([jsonDirectory jsonFileStr])
+            jsonFile = jsonFileStr;
+        end
+    end
+    jsonPath = jsonDirectory;
+    dt=jsondecode(fileread([jsonPath jsonFile]));
+    % GRABS RAW PIXEL SEPARATIONS BETWEEN AUTOREFRACTOR BARS
+    x=[x; -1.*[(cell2mat(struct2cell(dt.ext_right_mu))-cell2mat(struct2cell(dt.ext_left_mu)))]];
+    y=[y; -1.*[(cell2mat(struct2cell(dt.ext_bottom_mu))-cell2mat(struct2cell(dt.ext_top_mu)))]];
+
+    % THIS BLOCK SORTS THE DATA BY TRIAL
+    t3=[t3; cell2mat(struct2cell(dt.time_totalsecs))];           
+end
+AFCp = AFCpAll;
+
+v0=[find(diff(t3)>1); length(t3)];
+i0=1;
+for k0=1:length(v0);
+   v1=i0:v0(k0);
+   x0{k0}=x(v1);
+   y0{k0}=y(v1);
+   i0=v0(k0)+1;
+end
+
+% FINDS AUTOREFRACTOR VALUES FOR 'REFERENCE' ACCOMMODATION (SOME
+% FLEXIBILITY HERE ABOUT HOW TO DEFINE THE REFERENCE)
+i0=find(AFCp.v00(:,1)==0);
+i0 = 1;
+x1=[]; y1=[];
+for k0=1:length(i0)
+ x1=[x1 x0{i0(k0)}'];
+ y1=[y1 y0{i0(k0)}'];  
+end
+x1 = x1(1:10);
+y1 = y1(1:10);
+
+% MAIN ANALYSIS
+uniqueConditions = unique([AFCp.rgb100 AFCp.rgb200 AFCp.v00],'rows');
+uniqueRGBvalues = unique([AFCp.rgb100 AFCp.rgb200],'rows');
+xScale = -2.87; % SCALE FACTOR CONVERTING PIXELS TO DIOPTERS
+yScale = -2.58; % SCALE FACTOR CONVERTING PIXELS TO DIOPTERS
+optDistScale = 0.8;
+timeSeries = {};
+rgbValuesAll = {};
+x3stack = zeros([length(AFCp.AFCv) 400]);
+y3stack = zeros([length(AFCp.AFCv) 400]);
+meanChangeXvec = [];
+meanChangeYvec = [];
+
+for i = 1:size(uniqueConditions,1)
+    % INDEX OF COLOR CONDITIONS
+    indCnd = find(ismember([AFCp.rgb100 AFCp.rgb200 AFCp.v00],uniqueConditions(i,:),'rows'));
+    if bEXCLUDE
+       indCnd = indCnd(~ismember(indCnd,excludeTrials));
+    end
+    clear x2 y2 x3 y3;
+    x3=[]; y3=[]; 
+    rgbValues = [];
+    trialMarkerForPlot = [];
+    for k0=1:size(indCnd,1)
+       % ANALYZING SUBJECT'S ACCOMMODATION
+       x2{k0}=x0{indCnd(k0)}-mean(x1); % MEAN CENTERING
+       y2{k0}=y0{indCnd(k0)}-mean(y1); % MEAN CENTERING
+       x3tmp = (x2{k0}')./xScale;
+       y3tmp = (y2{k0}')./yScale;
+       % REMOVING OUTLIERS
+       x3diff = [0 diff(x3tmp)];
+       y3diff = [0 diff(y3tmp)];
+       x3outliers = abs(x3diff)>1 | abs(x3tmp)>5;
+       y3outliers = abs(y3diff)>1 | abs(y3tmp)>5;
+       meanx3 = mean(x3tmp(~x3outliers));
+       meany3 = mean(y3tmp(~y3outliers));
+       if bEXCLUDE
+           x3tmp(x3outliers) = meanx3;
+           y3tmp(y3outliers) = meany3;
+       end       
+       x3=[x3 x3tmp];
+       y3=[y3 y3tmp];
+       x3stack(indCnd(k0),1:length(x3tmp)) = x3tmp;
+       y3stack(indCnd(k0),1:length(y3tmp)) = y3tmp;
+       trialMarkerForPlot(k0) = length(x3);
+       % ACCOMMODATIVE DEMAND FROM EXPERIMENT 
+       sinValuesTmp = AFCp.sinValues(indCnd(k0),:);
+       tSin = 0:(1/(length(sinValuesTmp)-1)):1;
+       tSinInterp = 0:(1/(length(x2{k0})-1)):1;
+       accContinuous = interp1(tSin,sinValuesTmp,tSinInterp); 
+       % THIS IS AN OBNOXIOUS WAY OF COMPUTING THE AVERAGE CHANGE WITHIN A
+       % TRIAL, BUT IT WORKS AND MAY BE A BIT MORE ROBUST
+       diffVec = imresize([-2/length(accContinuous) 2/length(accContinuous)],size(accContinuous),'nearest');
+       if abs(corr(accContinuous',diffVec'))<0.95
+           error('ARCnlz_linearModel: you may want to check whether the step change occurs halfway through the trial, or not!');
+       end
+       meanChangeX(i,k0) = sum(diffVec.*x2{k0}')./xScale;
+       meanChangeY(i,k0) = sum(diffVec.*y2{k0}')./yScale;
+       % VECTOR OF RGB VALUES FOR PLOTTING
+       rgbValues = [rgbValues imresize([AFCp.rgb100(indCnd(k0),:)' AFCp.rgb200(indCnd(k0),:)'],[3 length(tSinInterp)],'nearest')];
+    end
+
+    % STORE ACCOMMODATION AND COLOR VALUES FOR PLOTTING
+    timeSeries{i,1} = x3;
+    timeSeries{i,2} = y3;
+    rgbValuesAll{i} = rgbValues;
+    trialMarkerForPlotCell{i} = trialMarkerForPlot;
+    indCndCell{i} = indCnd;
+    meanChangeXvec(indCnd) = meanChangeX(i,:);
+    meanChangeYvec(indCnd) = meanChangeY(i,:);
+end
+
+end
