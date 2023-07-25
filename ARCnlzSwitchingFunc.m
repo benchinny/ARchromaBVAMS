df1a9edc22d8d878b8ee6e631a70cebc3385fd6b
Ben Chin <benchin@Ascutney.local>
2023-07-25 14:54:06 -0700

    better attempt at implementing switching model

--- ARCnlzSwitchingFunc.m
+++ ARCnlzSwitchingFunc.m
@@ -1,20 +1,19 @@
 function RMSE = ARCnlzSwitchingFunc(deltaA,deltaR,deltaB,deltaS,p)
 
 wR=p(1);
-wB=p(2);
-wS=p(3);
-bias=p(4);
-rbThreshold=p(5);
+wS=p(2);
+bias=p(3);
+rbThreshold=p(4);
 
 wR = wR.*ones(size(deltaA));
-wB = wB.*ones(size(deltaA));
+wB = -wR.*ones(size(deltaA));
 wS = wS.*ones(size(deltaA));
 bias = bias.*ones(size(deltaA));
 
 wB(deltaR-deltaB>rbThreshold)=0;
 wR(deltaR-deltaB<=rbThreshold)=0;
 
-deltaApredicted = wR.*deltaR + wB.*deltaB + wS.*deltaS + bias;
+deltaApredicted = wR + wB + wS.*deltaS + bias;
 RMSE = sqrt(mean((deltaApredicted-deltaA).^2));
 
 end
--- ARCpowerAnalysis.m
+++ ARCpowerAnalysis.m
@@ -66,3 +66,8 @@ set(gca,'XTick',[1.5 3.5 5.5 7.5 9.5]);
 set(gca,'XTickLabel',{'1' '2' '3' '4' '5'});
 legend({'Accommodative demand' 'Color'});
 
+%% COMPARE WEIGHTING MODEL AND SWITCHING MODEL
+
+weightingModelRho = [0.946 0.863 0.939 0.943 0.528 0.971 0.853];
+switchingModelRho = [];
+
