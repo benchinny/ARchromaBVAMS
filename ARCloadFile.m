e98953cca2c3804745e13a86af12ed62a19fe59c
Benjamin Chin <benjaminchin@wifi-10-40-7-122.wifi.berkeley.edu>
2024-06-03 17:30:35 -0700

    new function for loading data

---
+++ ARCloadFile.m
@@ -0,0 +1,33 @@
+function [defocus, strehl, TimeStamp] = ARCloadFile(subjName,blockNum,trialNum,bExtended)
+
+defocus = [];
+strehl = [];
+
+dataFolder = '/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/CIVO_BVAMS/data/csvFiles/';
+
+dirAll = dir(dataFolder);
+
+filenamesAll = {dirAll.name};
+
+filenameTmp = [subjName '-Block_' num2str(blockNum) '-Trial_' num2str(trialNum)];
+
+if bExtended % IF USING EXTENDED ANALYSIS
+    ind = find(contains(filenamesAll,filenameTmp) & contains(filenamesAll,'FullPupil'));
+else
+    ind = find(contains(filenamesAll,filenameTmp) & ~contains(filenamesAll,'FullPupil'));
+end
+
+filename = filenamesAll{ind};
+
+trialData = readtable([dataFolder filename]);
+
+if bExtended
+   strehl = trialData.Defocus_MaxStrehl;
+   defocus = trialData.RMS_LCACorrected_;
+else
+   defocus = trialData.Defocus_LCACorrected_;
+end
+
+TimeStamp = trialData.TimeStamp;
+
+end
