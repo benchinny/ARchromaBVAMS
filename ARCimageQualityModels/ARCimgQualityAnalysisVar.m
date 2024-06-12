e12f811de236f6527cb700bf307efab83378a018
Benjamin Chin <benjaminchin@Coopers-MacBook-Pro.local>
2024-06-12 07:10:37 -0700

    more reorganizing of new image quality analysis function

--- ARCimageQualityModels/ARCimgQualityAnalysisVar.m
+++ ARCimageQualityModels/ARCimgQualityAnalysisVar.m
@@ -29,6 +29,18 @@ load T_xyz1931; % load color matching functions
 T_sensorXYZ = 683*SplineCmf(S_xyz1931,T_xyz1931,S); % interpolate and scale
 wave = S(1):S(2):S(1)+S(2)*(S(3)-1); % define wavelength vector
 
+% NEURAL SENSITIVITY FUNCTION FOR VISUAL STREHL RATIO CALCULATION
+oi = oiCreateARC('human',wave,0); % create optics
+% % making 2D CSF function
+[fx, fy] = meshgrid(oi.optics.OTF.fx,oi.optics.OTF.fy);
+% % scale so frequencies are in units of cyc/deg
+fx = fx./3.37;
+fy = fy./3.37;
+df = sqrt(fx.^2 + fy.^2); % compute distance from origin
+CSF2d = 0.04992*(1+5.9375*df).*exp(-0.114*df.^1.1);
+% inverse Fourier transform of 2D CSF
+N = ifftshift(ifft2(fftshift(CSF2d)));
+
 % Ben's stimulus
 nDotsI = 320;
 rVal = 0.56;
@@ -68,18 +80,6 @@ vcAddObject(s);
 
 %% Computing visual Strehl ratio
 
-oi = oiCreateARC('human',wave,0); % create optics
-
-% % making 2D CSF function
-[fx, fy] = meshgrid(oi.optics.OTF.fx,oi.optics.OTF.fy);
-% % scale so frequencies are in units of cyc/deg
-fx = fx./3.37;
-fy = fy./3.37;
-df = sqrt(fx.^2 + fy.^2); % compute distance from origin
-CSF2d = 0.04992*(1+5.9375*df).*exp(-0.114*df.^1.1);
-% inverse Fourier transform of 2D CSF
-N = ifftshift(ifft2(fftshift(CSF2d)));
-
 Dall = -humanWaveDefocus(380:5:780); % defocus values to look at
 peakPSF = [];
 polyPSFall = [];
@@ -109,17 +109,11 @@ for i = 1:length(Dall)
 end
 
 figure; 
-% plot(humanWaveDefocusInvert(-Dall),vsx./max(vsx),'k-','LineWidth',1); hold on;
 plot(humanWaveDefocusInvert(-Dall),0.29.*peakPSF./max(peakPSF),'k-','LineWidth',1);
-% legend('Visual Strehl','Strehl');
 axis square;
 set(gca,'FontSize',15);
 xlabel('Wavelength in focus');
 ylabel('Ratio');
-% ind = 21; % examine at particular wavelength index
-% testWave = oi.optics.OTF.wave(ind);
-% testOTF = fftshift(oi.optics.OTF.OTF(:,:,ind));
-% testPSF = ifftshift(ifft2(oi.optics.OTF.OTF(:,:,ind)));
 
 %% Turning original stimulus into luminance image
 
@@ -182,20 +176,4 @@ axis square;
 set(gca,'FontSize',15);
 xlabel('Wavelength in focus');
 ylabel('Peak correlation');
-
-%%
-
-lumRed = [0.25 0.5 0.75 1];
-lumBlue = [0.25 0.5 0.75 1];
-bestFocusLambdaFullRedCorr = [600 600 580 570];
-bestFocusLambdaFullBlueCorr = [500 520 545 570];
-
-figure;
-hold on;
-plot(lumRed,bestFocusLambdaFullBlueCorr,'-ro','LineWidth',1.5,'MarkerSize',12);
-plot(lumBlue,bestFocusLambdaFullRedCorr,'-bo','LineWidth',1.5,'MarkerSize',12);
-axis square;
-formatFigure('Relative Luminance','Wavelength in focus','Correlation metric');
-ylim([400 700]);
-
 
