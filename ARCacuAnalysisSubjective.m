8f60278c378abf689dfcc8a570484ddd3c2266b8
Ben Chin <ben.chinny@gmail.com>
2024-10-08 13:24:17 -0700

    small updates to analysis functions

--- ARCacuAnalysisSubjective.m
+++ ARCacuAnalysisSubjective.m
@@ -1,5 +1,5 @@
 %%
-
+function ARCacuAnalysisSubjective
 % filePath = 'G:\My Drive\exp_bvams\code_repo\ARC\';
 filePath = 'H:\Shared drives\CIVO_BVAMS\data\ARC\';
 
@@ -23,7 +23,7 @@ if subjNum==3
                   };
 elseif subjNum==9
     filenames = {
-                  % S9 PURPLE
+                  % S10 PURPLE
                   [dataDirectory 'S1019V9_AFC_RightACL0_2410041308.mat'] ...
                   [dataDirectory 'S1019V9_AFC_RightACL0_2410041301.mat'] ...
                   [dataDirectory 'S1019V9_AFC_RightACL0_2410041254.mat'] ...
@@ -37,9 +37,9 @@ elseif subjNum==10
                   [dataDirectory 'S1020V10_AFC_RightACL0_2409111607.mat'] ...
                   [dataDirectory 'S1020V10_AFC_RightACL0_2409111615.mat'] ...
                   };
-elseif subjNum==5
+elseif subjNum==15
     filenames = {
-                  % S5 PURPLE
+                  % S10 PURPLE
                   [dataDirectory 'S1015V10_AFC_RightACL0_2410031638.mat'] ...
                   [dataDirectory 'S1015V10_AFC_RightACL0_2410031631.mat'] ...
                   [dataDirectory 'S1015V10_AFC_RightACL0_2410031615.mat'] ...
@@ -262,3 +262,4 @@ errorbar(unqFocDst.*scaleFac,PC,PC-PCci(1,:),PCci(2,:)-PC,'o-','Color',rgbUnq,'M
 axis square;
 ylim([0.4 1]);
 formatFigure('Relative optical distance (D)','Proportion Correct',['Subject ' num2str(subjNum)]);
+end
