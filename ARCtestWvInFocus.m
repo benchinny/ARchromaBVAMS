3cc5a1f75d9497909b7ec189864dd1ef1806103f
Benjamin Chin <benjaminchin@Coopers-MacBook-Pro.local>
2024-10-08 21:55:12 -0700

    cone weighting analysis almost working?

--- ARCtestWvInFocus.m
+++ ARCtestWvInFocus.m
@@ -1,15 +1,34 @@
 %%
 
-subjNum = 1;
-trialNumAll = 1:20;
-wLMSall = [0.8 0.1 0.1; 0.1 0.8 0.1; 0.1 0.1 0.8];
+subjNum = 10;
+blockNumAll = 3:8;
+trialNumAll = 1:36;
+wLunq = 0:0.1:1;
+wMunq = 0:0.1:1;
 
-for i = 1:size(wLMSall,1)
-    wLMS = wLMSall(i,:);
-    for j = 1:length(trialNumAll)
-        trialNum = trialNumAll(j);
-        wvInFocus(i,j) = ARCwvInFocusCones(subjNum,trialNum,wLMS);
-        display(['Weight set ' num2str(i) ', trial number ' num2str(j)]);
+for i = 1:length(wLunq)
+    for j = 1:length(wMunq)
+        wL = wLunq(i);
+        wM = wMunq(j);
+        wS = 1-wL-wM;
+        for k = 1:length(blockNumAll)
+            for l = 1:length(trialNumAll)
+                wvInFocus(k,l) = ARCwvInFocusCones(subjNum,blockNumAll(k),trialNumAll(l),[wL wM wS]);
+                display(['wLind = ' num2str(wLunq(i)) ', wMind = ' num2str(wMunq(j)) ', block number ' num2str(blockNumAll(k)) ', trial number ' num2str(trialNumAll(l))]);  
+                % LOAD ZERNIKE TABLE AND TIMESTAMPS
+                [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumTmp,trialNumTmp,0);
+        
+                NumCoeffs = width(ZernikeTable)-8; % determine how many coefficients are in the cvs file. 
+                c=zeros(size(ZernikeTable,1),65); %this is the vector that contains the Zernike polynomial coefficients. We can work with up to 65. 
+                PARAMS = struct;
+                PARAMS.PupilSize=mean(table2array(ZernikeTable(:,5))); %default setting is the pupil size that the Zernike coeffs define, PARAMS(3)
+                PARAMS.PupilFitSize=mean(table2array(ZernikeTable(:,5))); 
+                PARAMS.PupilFieldSize=PARAMS.PupilSize*2; %automatically compute the field size
+                c(:,3:NumCoeffs)=table2array(ZernikeTable(:,11:width(ZernikeTable)));
+                indBad = c(:,4)==0;
+                meanC = mean(c(~indBad,:),1); % TAKE MEAN OF COEFFICIENTS                
+            end
+        end
     end
 end
 
--- ARCwvInFocusCones.m
+++ ARCwvInFocusCones.m
@@ -1,19 +1,19 @@
-function wvInFocus = ARCwvInFocusCones(subjNum,trialNum,wLMS)
+function wvInFocus = ARCwvInFocusCones(subjNum,blockNum,trialNum,wLMS)
 
 wave = 380:4:780;
 wave = wave(16:101);
 nFocus = length(wave);
 foldernameCones = '/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/CIVO_BVAMS/data/coneImages/';
 
-fnameConeRspNoLCA = ['subj' num2str(subjNum) 'stimulus' num2str(trialNum) 'focusInd6noLCA'];
-absorptionsOrig = load([foldernameCones fnameConeRspNoLCA]);
+fnameConeRspNoLCA = ['subj' num2str(subjNum) 'block1stimulus1' 'focusInd1noLCA'];
+absorptionsOrig = load([foldernameCones 'S' num2str(subjNum) '/' fnameConeRspNoLCA]);
 absorptionsOrig = absorptionsOrig.absorptions;
 coneImgOrig = sum(absorptionsOrig,3);
 peakCorr = [];
 
 for i = 1:nFocus
-    fnameConeRsp = ['subj' num2str(subjNum) 'stimulus' num2str(trialNum) 'focusInd' num2str(i)];
-    load([foldernameCones fnameConeRsp]);
+    fnameConeRsp = ['subj' num2str(subjNum) 'block' num2str(blockNum) 'stimulus' num2str(trialNum) 'focusInd' num2str(i)];
+    load([foldernameCones 'S' num2str(subjNum) '/' fnameConeRsp]);
     absorptions(:,:,1) = absorptions(:,:,1).*wLMS(1);
     absorptions(:,:,2) = absorptions(:,:,2).*wLMS(2);
     absorptions(:,:,3) = absorptions(:,:,3).*wLMS(3);
