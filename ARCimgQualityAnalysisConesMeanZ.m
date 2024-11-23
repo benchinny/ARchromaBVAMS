764c9ccf249f4f8a520a10dd1d22a99404412ffd
Benjamin Chin <benjaminchin@wifi-10-40-5-180.wifi.berkeley.edu>
2024-11-22 16:24:37 -0800

    cone analysis averaging all coefficients

--- ARCimgQualityAnalysisConesMeanZ.m
+++ ARCimgQualityAnalysisConesMeanZ.m
@@ -5,6 +5,7 @@ ieInit;
 
 subjNum = 10;
 subjNumEncode = subjNum+10;
+bPlotCoefficients = false;
 
 % Setting up display properties
 d = displayCreate('OLED-Samsung');
@@ -48,14 +49,6 @@ Dall = -humanWaveDefocus(wave);
 % WAVELENGTHS TO LOOK AT
 % wvAll = humanWaveDefocusInvert(-Dall);
 
-% PARAMETERS OF WAVEFRONT ANALYSIS
-PARAMS.PixelDimension = 512;% size of pupil aperture field in pixels (this defines the resolution of the calculation)
-PARAMS.PupilSize = 7; %default values - will be replaced depending on choices below
-PARAMS.PupilFieldSize =42; %default values - will be replaced depending on choices below
-PARAMS.PupilFitSize = 7; %default values - will be replaced depending on choices below
-PARAMS.ImagingWavelength = 0.55;% imaging wavelength in microns
-PARAMS.WavefrontResolution = 53;% increase to enhance the display of the wavefront (doesn't affect calculation)
-
 %%
 
 if subjNum==3
@@ -77,14 +70,17 @@ end
 %%
 
 cAll = [];
+optDistAll = [];
+rgbAll = [];
 
 for l = 1:length(blockNums) % LOOP OVER BLOCK
+    blockNumInd = l;
+    blockNumTmp = blockNums(blockNumInd);
+    AFCp = ARCloadFileBVAMS(subjNumEncode,blockNumTmp); % LOAD BVAMS DATA
+    rgbAll = [rgbAll; AFCp.rgb100];
     for k = 1:36 % LOOP OVER TRIAL
-        blockNumInd = l;
-        blockNumTmp = blockNums(blockNumInd);
         trialNumTmp = trialNums(k,blockNumInd);
         
-        AFCp = ARCloadFileBVAMS(subjNumEncode,blockNumTmp); % LOAD BVAMS DATA
         % LOAD ZERNIKE TABLE AND TIMESTAMPS
         [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumTmp,trialNumTmp,0);
 
@@ -95,149 +91,204 @@ for l = 1:length(blockNums) % LOOP OVER BLOCK
         PARAMS.PupilFitSize=mean(table2array(ZernikeTable(:,5))); 
         PARAMS.PupilFieldSize=PARAMS.PupilSize*2; %automatically compute the field size
         c(:,3:NumCoeffs)=table2array(ZernikeTable(:,11:width(ZernikeTable)));
+        optDistTmp = (AFCp.meanv00(k)./1.2255).*ones([size(c,1) 1]);
+        optDistAll = [optDistAll; optDistTmp];
         cAll = [cAll; c];
     end
 end
 
 indBad = cAll(:,4)==0;
-% meanC = mean(c(~indBad,:),1); % TAKE MEAN OF COEFFICIENTS
+meanC = mean(cAll(~indBad,:),1); % TAKE MEAN OF COEFFICIENTS
+rgb00 = unique(rgbAll,'rows');
+
+%% PLOTTING EACH COEFFICIENT VS DEFOCUS 
+
+if bPlotCoefficients
+    figure;
+    set(gcf,'Position',[163 358 1304 588]);
+    for i = 1:10
+        subplot(2,5,i);
+        hist(cAll(~indBad,i+2),linspace(-0.5,2,101));
+        axis square;
+    end
+    
+    figure;
+    set(gcf,'Position',[117 149 1393 773]);
+    subplot(2,1,1);
+    hold on;
+    for i = 1:65
+        bar(i,mean(cAll(~indBad,i)));
+    end
+    formatFigure('Zernike polynomial #','Mean coefficient');
+    subplot(2,1,2);
+    hold on;
+    for i = 1:65
+        bar(i,std(cAll(~indBad,i)));
+    end
+    formatFigure('Zernike polynomial #','Std dev coefficient');
+    
+    coeffs2compare = [3 5:13];
+    figure;
+    set(gcf,'Position',[117 149 1393 773]);
+    for i = 1:length(coeffs2compare)
+       subplot(2,5,i);
+       plot(cAll(~indBad,4),cAll(~indBad,coeffs2compare(i)),'k.');
+       % ylim([-0.25 0.25]);
+       axis square;
+       if i==1
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Astigmatism');
+       elseif i==2
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Astigmatism');
+       elseif i==3
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Trefoil 0');
+       elseif i==4
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Coma Y');  
+       elseif i==5
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Coma X');
+       elseif i==6
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Trefoil 30');
+       elseif i==7 || i==8
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'');
+       elseif i==9
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))],'Spherical aberration');      
+       else
+          formatFigure('Defocus coefficient',['Coefficient ' num2str(coeffs2compare(i))]);
+       end
+    end
+
+    % PLOTTING EACH COEFFICIENT VS OPTICAL DISTANCE
+    
+    optDistUnq = unique(optDistAll);
+    
+    figure;
+    set(gcf,'Position',[117 149 1393 773]);
+    for i = 1:length(coeffs2compare)
+       subplot(2,5,i);
+       hold on;
+       for j = 1:length(optDistUnq)
+           indOptDist = abs(optDistAll-optDistUnq(j))<0.001;
+           errorbar(optDistUnq(j),mean(cAll(~indBad & indOptDist,coeffs2compare(i))), ...
+                    std(cAll(~indBad & indOptDist,coeffs2compare(i))),'ko', ...
+                    'MarkerSize',10,'MarkerFaceColor','w','LineWidth',1);
+       end
+       axis square;
+       xlim([1 4]);
+       if i==1
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Astigmatism');
+       elseif i==2
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Astigmatism');
+       elseif i==3
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Trefoil 0');
+       elseif i==4
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Coma Y');  
+       elseif i==5
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Coma X');
+       elseif i==6
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Trefoil 30');
+       elseif i==7 || i==8
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'');
+       elseif i==9
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))],'Spherical aberration');      
+       else
+          formatFigure('Optical distance (D)',['Coefficient ' num2str(coeffs2compare(i))]);
+       end   
+    end
+end
 
 %%
 
-figure;
-set(gcf,'Position',[163 358 1304 588]);
-for i = 1:10
-    subplot(2,5,i);
-    hist(cAll(~indBad,i+2),linspace(-0.5,2,101));
-    axis square;
-end
+for k = 1:size(rgb00,1) % LOOP OVER TRIAL
+    % recreate stimulus
+    rVal = rgb00(k,1);
+    gVal = rgb00(k,2);
+    bVal = rgb00(k,3);
+    im = imread([stimPath '/word_image_01.png']);
+    im = double(im);
+    imPattern = squeeze(im(:,:,3));
+    imPattern = [zeros([60 size(imPattern,2)]); imPattern; zeros([60 size(imPattern,2)])];
+    imPattern = [zeros([size(imPattern,1) 30]) imPattern zeros([size(imPattern,1) 30])];
+    I(:,:,3) = bVal.*imPattern;
+    I(:,:,2) = gVal.*imPattern;
+    I(:,:,1) = rVal.*imPattern;
+    I = I./255;
+    
+    % Turn image into 'scene'
+    s = sceneFromFile(I, 'rgb', [], d);  % The display is included here
+    % I think this copies the struct into an object
+    vcAddObject(s); 
+    
+    % figure; 
+    % set(gcf,'Position',[289 428 1056 420]);
+    % subplot(1,3,1);
+    % plot(d.wave,d.spd(:,1),'r','LineWidth',1.5); hold on;
+    % plot(d.wave,d.spd(:,2),'g','LineWidth',1.5);
+    % plot(d.wave,d.spd(:,3),'b','LineWidth',1.5);
+    % axis square;
+    % formatFigure('Wavelength (\lambda)','Radiance');
+    % subplot(1,3,2);
+    % imagesc(I);
+    % set(gca,'XTick',[]);
+    % set(gca,'YTick',[]);
+    % axis square;
+    % set(gca,'FontSize',15);
+    % title('Original');
+    % subplot(1,3,3);
+    % plot(s.spectrum.wave,squeeze(s.data.photons(160,160,:)),'-k','LineWidth',1);
+    % formatFigure('Wavelength (\lambda)','Photons');
+    % axis square;
+    
+    %% Computing peak correlation for different wavelengths in focus
+    
+    wave2 = 380:4:780;
 
-figure;
-set(gcf,'Position',[117 149 1393 773]);
-subplot(2,1,1);
-hold on;
-for i = 1:65
-    bar(i,mean(cAll(~indBad,i)));
-end
-formatFigure('Zernike polynomial #','Mean coefficient');
-subplot(2,1,2);
-hold on;
-for i = 1:65
-    bar(i,std(cAll(~indBad,i)));
-end
-formatFigure('Zernike polynomial #','Std dev coefficient');
-
-%%
-
-for l = 3 % LOOP OVER BLOCK
-    for k = 1:36 % LOOP OVER TRIAL
-        % LOADING DATA
-        blockNumInd = l;
-        blockNumTmp = blockNums(blockNumInd);
-        trialNumTmp = trialNums(k,blockNumInd);
-        
-        AFCp = ARCloadFileBVAMS(subjNumEncode,blockNumTmp); % LOAD BVAMS DATA
-        % LOAD ZERNIKE TABLE AND TIMESTAMPS
-        [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumTmp,trialNumTmp,0);
-
-        NumCoeffs = width(ZernikeTable)-8; % determine how many coefficients are in the cvs file. 
-        c=zeros(size(ZernikeTable,1),65); %this is the vector that contains the Zernike polynomial coefficients. We can work with up to 65. 
-        PARAMS = struct;
-        PARAMS.PupilSize=mean(table2array(ZernikeTable(:,5))); %default setting is the pupil size that the Zernike coeffs define, PARAMS(3)
-        PARAMS.PupilFitSize=mean(table2array(ZernikeTable(:,5))); 
-        PARAMS.PupilFieldSize=PARAMS.PupilSize*2; %automatically compute the field size
-        c(:,3:NumCoeffs)=table2array(ZernikeTable(:,11:width(ZernikeTable)));
-        indBad = c(:,4)==0;
-        meanC = mean(c(~indBad,:),1); % TAKE MEAN OF COEFFICIENTS
-        
-        % STORE COLORS FOR FIRST AND SECOND STIMULI
-        rgb00 = AFCp.rgb100(trialNumTmp,:);
-
-        % recreate stimulus
-        rVal = rgb00(1,1);
-        gVal = rgb00(1,2);
-        bVal = rgb00(1,3);
-        im = imread([stimPath '/word_image_01.png']);
-        im = double(im);
-        imPattern = squeeze(im(:,:,3));
-        imPattern = [zeros([60 size(imPattern,2)]); imPattern; zeros([60 size(imPattern,2)])];
-        imPattern = [zeros([size(imPattern,1) 30]) imPattern zeros([size(imPattern,1) 30])];
-        I(:,:,3) = bVal.*imPattern;
-        I(:,:,2) = gVal.*imPattern;
-        I(:,:,1) = rVal.*imPattern;
-        I = I./255;
-        
-        % Turn image into 'scene'
-        s = sceneFromFile(I, 'rgb', [], d);  % The display is included here
-        % I think this copies the struct into an object
-        vcAddObject(s); 
-        
-        % figure; 
-        % set(gcf,'Position',[289 428 1056 420]);
-        % subplot(1,3,1);
-        % plot(d.wave,d.spd(:,1),'r','LineWidth',1.5); hold on;
-        % plot(d.wave,d.spd(:,2),'g','LineWidth',1.5);
-        % plot(d.wave,d.spd(:,3),'b','LineWidth',1.5);
-        % axis square;
-        % formatFigure('Wavelength (\lambda)','Radiance');
-        % subplot(1,3,2);
-        % imagesc(I);
-        % set(gca,'XTick',[]);
-        % set(gca,'YTick',[]);
-        % axis square;
-        % set(gca,'FontSize',15);
-        % title('Original');
-        % subplot(1,3,3);
-        % plot(s.spectrum.wave,squeeze(s.data.photons(160,160,:)),'-k','LineWidth',1);
-        % formatFigure('Wavelength (\lambda)','Photons');
-        % axis square;
-        
-        %% Computing peak correlation for different wavelengths in focus
-        
-        Dall2 = -humanWaveDefocus(wave(1:101));
-
-        for i = 46 % 1:length(Dall2)
-            zCoeffs = [0 meanC(1:end-1)];
-            wvfP = wvfCreate('calc wavelengths', wave, ...
-                'measured wavelength', humanWaveDefocusInvert(-Dall2(i)), ...
-                'zcoeffs', zCoeffs, 'measured pupil', PARAMS.PupilSize, ...
-                'name', sprintf('human-%d', PARAMS.PupilSize),'spatial samples',size(im,2));
-            wvfP.calcpupilMM = PARAMS.PupilSize;
-            defocusFromLCA = max(abs([humanWaveDefocusS10(humanWaveDefocusInvert(-Dall2(i)),min(wave)) ...
-                              humanWaveDefocusS10(humanWaveDefocusInvert(-Dall2(i)),max(wave))]));
-            if defocusFromLCA<1
-                wvfP.refSizeOfFieldMM = 12;
-            else
-                wvfP.refSizeOfFieldMM = 6;
-            end
-            wvfP = wvfSet(wvfP, 'zcoeff', 0, 'defocus');
-            wvfP = wvfSet(wvfP, 'customlca', @humanWaveDefocusS10);
-            
-            % Convert to siData format as well as wavefront object
-            [siPSFData, wvfP] = wvf2SiPsfARC(wvfP,'showBar',false,'nPSFSamples',size(im,2),'umPerSample',1.1512); 
-            oi = wvf2oi(wvfP); % CONVERT TO OPTICS OBJECT
-            oi.optics.OTF.OTF = siPSFData.otf;
-            % oi = oiCreateARC('human',wave,Dall(i)); % create optics
-            oi = oiCompute(oi, s); % compute optical image of stimulus
-        
-            % Create the coneMosaic object
-            cMosaic = coneMosaic;
-
-            % Set size to show relevant portion of scene
-            cMosaic.setSizeToFOV(1 * sceneGet(s, 'fov'));
-
-            % key line for computing absorptions
-            absorptions = cMosaic.computeSingleFrame(oi, 'fullLMS', true);            
-            
-            % absorptions = absorptions(55:128,6:177,:);
-
-            display(['Peak correlation loop ' num2str(i) ' block ' num2str(blockNumTmp) ' trial ' num2str(k)]);
-            absorptions = single(absorptions);
-            S = struct;
-            S.absorptions = absorptions;
-            fnameCone = ['subj' num2str(subjNum) 'block' num2str(blockNumTmp) 'stimulus' num2str(k) 'focusInd' num2str(i)];
-            save([savePath num2str(subjNum) '/' fnameCone '.mat'],"-fromstruct",S);
+    parfor i = 1:length(wave2)
+        zCoeffs = [0 meanC(1:end-1)];
+        wvfP = wvfCreate('calc wavelengths', wave, ...
+            'measured wavelength', wave2(i), ...
+            'zcoeffs', zCoeffs, 'measured pupil', PARAMS.PupilSize, ...
+            'name', sprintf('human-%d', PARAMS.PupilSize),'spatial samples',size(im,2));
+        wvfP.calcpupilMM = PARAMS.PupilSize;
+        defocusFromLCA = max(abs([humanWaveDefocusS10(wave2(i),min(wave)) ...
+                                  humanWaveDefocusS10(wave2(i),max(wave))]));
+        if defocusFromLCA<1
+            wvfP.refSizeOfFieldMM = 12;
+        else
+            wvfP.refSizeOfFieldMM = 6;
         end
+        wvfP = wvfSet(wvfP, 'zcoeff', 0, 'defocus');
+        wvfP = wvfSet(wvfP, 'customlca', @humanWaveDefocusS10);
+        
+        % Convert to siData format as well as wavefront object
+        [siPSFData, wvfP] = wvf2SiPsfARC(wvfP,'showBar',false,'nPSFSamples',size(im,2),'umPerSample',1.1512); 
+        oi = wvf2oi(wvfP); % CONVERT TO OPTICS OBJECT
+        paddingXCpsf = round((size(siPSFData.psf,2)-size(s.data.photons,2))/2);
+        paddingYRpsf = round((size(siPSFData.psf,1)-size(s.data.photons,1))/2);
+        indNotPadded = {(paddingYRpsf+1):(size(siPSFData.psf,1)-paddingYRpsf) ...
+                        (paddingXCpsf+1):(size(siPSFData.psf,2)-paddingXCpsf)};
+        oi.optics.OTF = [];
+        for j = 1:size(siPSFData.psf,3)
+            oi.optics.OTF.OTF(:,:,j) = fft2(fftshift(squeeze(siPSFData.psf(indNotPadded{1},indNotPadded{2},j))));
+        end
+        % oi = oiCreateARC('human',wave,Dall(i)); % create optics
+        oi = oiCompute(oi, s); % compute optical image of stimulus
+    
+        % Create the coneMosaic object
+        cMosaic = coneMosaic;
+
+        % Set size to show relevant portion of scene
+        cMosaic.setSizeToFOV(1 * sceneGet(s, 'fov'));
+
+        % key line for computing absorptions
+        absorptions = cMosaic.computeSingleFrame(oi, 'fullLMS', true);            
+        
+        % absorptions = absorptions(55:128,6:177,:);
+
+        display(['Peak correlation loop ' num2str(i) ' block ' num2str(blockNumTmp) ' trial ' num2str(k)]);
+        absorptions = single(absorptions);
+        S = struct;
+        S.absorptions = absorptions;
+        fnameCone = ['subj' num2str(subjNum) 'stimulus' num2str(k) 'focusInd' num2str(i)];
+        save([savePath num2str(subjNum) '/' fnameCone '.mat'],"-fromstruct",S);
     end
 end
 
