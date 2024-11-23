808d5b90cda80b70b13e0605524d347e6b893ea1
Benjamin Chin <benjaminchin@Coopers-MacBook-Pro.local>
2024-11-23 10:21:14 -0800

    moving towards faster cone weight analysis with wavefront averaged across entire experiment

--- ARCtestWvInFocusMeanZ.m
+++ ARCtestWvInFocusMeanZ.m
@@ -6,9 +6,9 @@ if subjNum==10
 end
 blockNumAll = 3:8;
 trialNumAll = 1:36;
-wLunq = -1:0.2:1;
-wMunq = -1:0.2:1;
-wSunq = [-0.6 0.2];
+wLunq = 1;
+wMunq = 1;
+wSunq = 0;
 RMSE = [];
 defocus875all = [];
 defocus875predAll = [];
@@ -23,36 +23,23 @@ for Sindex = 1:length(wSunq)
             wS = wSunq(Sindex);
             defocus875 = zeros([length(blockNumAll) length(trialNumAll)]);
             defocus875pred = zeros([length(blockNumAll) length(trialNumAll)]); 
+            rgbAll = [];
+            optDistAll = [];
             for k = 1:length(blockNumAll)
-                parfor l = 1:length(trialNumAll)
-                    wvInFocus(k,l) = ARCwvInFocusCones(subjNum,blockNumAll(k),trialNumAll(l),[wL wM wS]);
-                    display(['wLind = ' num2str(wLunq(i)) ', wMind = ' num2str(wMunq(j)) ', wSind = ' num2str(wSunq(Sindex)) ', block number ' num2str(blockNumAll(k)) ', trial number ' num2str(trialNumAll(l))]);  
-                    % LOAD ZERNIKE TABLE AND TIMESTAMPS
-                    [ZernikeTable, ~, ~, TimeStamp] = ARCloadFileFIAT(subjName,blockNumAll(k),trialNumAll(l),0);
-            
-                    NumCoeffs = width(ZernikeTable)-8; % determine how many coefficients are in the cvs file. 
-                    c=zeros(size(ZernikeTable,1),65); %this is the vector that contains the Zernike polynomial coefficients. We can work with up to 65. 
-                    PARAMS = struct;
-                    PARAMS.PupilSize=mean(table2array(ZernikeTable(:,5))); %default setting is the pupil size that the Zernike coeffs define, PARAMS(3)
-                    PARAMS.PupilFitSize=mean(table2array(ZernikeTable(:,5))); 
-                    PARAMS.PupilFieldSize=PARAMS.PupilSize*2; %automatically compute the field size
-                    c(:,3:NumCoeffs)=table2array(ZernikeTable(:,11:width(ZernikeTable)));
-                    indBad = c(:,4)==0;
-                    meanC = mean(c(~indBad,:),1); % TAKE MEAN OF COEFFICIENTS    
-                    defocusCorrectionFactor = (1e6/(4*sqrt(3)))*((PARAMS.PupilSize/2000)^2);
-                    defocus875(k,l) = meanC(4)./defocusCorrectionFactor;
-                    AFCp = ARCloadFileBVAMS(subjNum+10,blockNumAll(k));
-                    defocus875pred(k,l) = AFCp.meanv00(trialNumAll(l))./1.2255 - humanWaveDefocusS10(wvInFocus(k,l),875);
-                end
+                AFCp = ARCloadFileBVAMS(subjNum+10,blockNumAll(k));
+                rgbAll = [rgbAll; AFCp.rgb100];
+                optDistAll = [optDistAll; AFCp.meanv00./1.2255];
             end
-            RMSE(i,j,Sindex) = sqrt(mean((defocus875pred(:)-defocus875(:)).^2));
-            defocus875predAll(:,:,i,j,Sindex) = defocus875pred;
-            defocus875all(:,:,i,j,Sindex) = defocus875;
-            wvInFocusAll(:,:,i,j,Sindex) = wvInFocus;
+            rgbUnq = unique(rgbAll,'rows');
         end
     end
    save([savePath 'wvInFocusModelResults' num2str(round(wSunq(Sindex)*10)) '.mat'],'defocus875all','defocus875predAll','wvInFocusAll','RMSE','wSunq'); 
 end
+
+% RMSE(i,j,Sindex) = sqrt(mean((defocus875pred(:)-defocus875(:)).^2));
+% defocus875predAll(:,:,i,j,Sindex) = defocus875pred;
+% defocus875all(:,:,i,j,Sindex) = defocus875;
+% wvInFocusAll(:,:,i,j,Sindex) = wvInFocus;
 
 % %%
 % 
