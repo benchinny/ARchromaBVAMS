bf2359fb6556af82d68724f00877ff0c03e16255
Ben Chin <ben.chinny@gmail.com>
2025-01-02 06:37:00 -0800

    add a method of incorporating L-S balance into image quality model

--- ARCwvInFocusConesMeanZbalance.m
+++ ARCwvInFocusConesMeanZbalance.m
@@ -1,4 +1,4 @@
-function wvInFocus = ARCwvInFocusConesMeanZbalance(subjNum,stimNum,wLMS)
+function wvInFocus = ARCwvInFocusConesMeanZbalance(subjNum,stimNum,wLMS,wQualityBalance)
 
 wave = 380:4:780;
 nFocus = length(wave);
@@ -12,18 +12,34 @@ absorptionsOrig = load([foldernameCones 'S10/' fnameConeRspNoLCA]);
 absorptionsOrig = absorptionsOrig.absorptions;
 coneImgOrig = sum(absorptionsOrig,3);
 peakCorr = [];
+LSbalance = [];
 
 for i = 1:nFocus
     fnameConeRsp = ['subj' num2str(subjNum) 'stimulus' num2str(stimNum) 'focusInd' num2str(i)];
     load([foldernameCones 'S' num2str(subjNum) '/' fnameConeRsp]);
-    absorptions(:,:,1) = absorptions(:,:,1).*wLMS(1);
-    absorptions(:,:,2) = absorptions(:,:,2).*wLMS(2);
-    absorptions(:,:,3) = absorptions(:,:,3).*wLMS(3);
-    coneImg = sum(absorptions,3);
+    % WEIGHT AND ADD CONE IMAGES
+    coneImg = squeeze(absorptions(:,:,1).*wLMS(1) + absorptions(:,:,2).*wLMS(2) + absorptions(:,:,3).*wLMS(3));
+    % PEAK CORRELATION OF 'QUALITY' IMAGE
     peakCorr(i) = max(max(normxcorr2(coneImgOrig,coneImg)));
+    % CORRELATING L AND S IMAGES WITH ORIGINAL
+    peakCorrL = max(max(normxcorr2(coneImgOrig,squeeze(absorptions(:,:,1)))));
+    peakCorrS = max(max(normxcorr2(coneImgOrig,squeeze(absorptions(:,:,3)))));
+    % CALCULATING DIFFERENCE IN IMAGE QUALITY BETWEEN L AND S IMAGES
+    LSbalance(i) = peakCorrL-peakCorrS;
 end
 
+% WAVELENGTH TO FOCUS IF MAXIMIZING IMAGE QUALITY
 [~,indPeakPeak] = max(peakCorr);
-wvInFocus = wave(indPeakPeak);
+wvInFocusQuality = wave(indPeakPeak);
+% WAVELENGTH TO FOCUS IF MAXIMIZING BALANCE
+[~,indPeakBalance] = max(LSbalance);
+wvInFocusBalance = wave(indPeakBalance);
+
+% CONVERT WAVELENGTHS TO FOCUS TO DIOPTERS, THEN WEIGHT AND ADD
+wvInFocusDiopters = wQualityBalance(1)*humanWaveDefocusARC(550,wvInFocusQuality,99) + ...
+                    wQualityBalance(2)*humanWaveDefocusARC(550,wvInFocusBalance,99);
+
+% REVERSE TO OBTAIN WAVELENGTH THAT SHOULD BE FOCUSED
+wvInFocus = humanWaveDefocusInvertARC(550,wvInFocusDiopters,99);
 
 end
