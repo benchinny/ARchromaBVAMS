630750b5ed03a3851ef634dde0d45bcd5197f652
Benjamin Chin <benjaminchin@Coopers-MacBook-Pro.local>
2025-01-03 15:47:05 -0800

    fixed bug in cone model outputs (didn't use customized LCA)

--- ARCtestWvInFocusMeanZ.m
+++ ARCtestWvInFocusMeanZ.m
@@ -1,6 +1,6 @@
 %%
 
-subjNum = 20;
+subjNum = 11;
 if subjNum==10
     subjName = 'S20-OD';
     blockNumAll = 3:8;
@@ -30,55 +30,6 @@ elseif subjNum==20
     blockNumAll = 2:7;
 end
 
-if subjNum==20
-    % RMSEall = zeros([11 11 11]);
-    % SvaluesAll = [-1 -0.8 -0.6 -0.4 -0.2 0.0 0.2 0.4 0.6 0.8 1];
-    % loadStr = {'-10' '-8' '-6' '-4' '-2' '0' '2' '4' '6' '8' '10'};
-    % blockNums = 3:8;
-    % ind2examine = 1:11;
-
-    % RMSEall = zeros([11 11 1]);
-    % SvaluesAll = [-0.8];
-    % loadStr = {'-8'};
-    % blockNums = 3:8;
-    % ind2examine = 1;    
-    
-    RMSEall = zeros([11 11 3]);
-    blockNums = 3:8;
-    ind2examine = 1:3;     
-elseif subjNum==13
-    RMSEall = zeros([11 11 3]);
-    blockNums = 12:17;
-    ind2examine = 1:3; 
-elseif subjNum==11
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 11:16;
-elseif subjNum==15
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 3:8;
-elseif subjNum==19
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 2:7;
-elseif subjNum==26
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 2:7;
-elseif subjNum==27
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 2:7;
-elseif subjNum==28
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 2:7;
-elseif subjNum==30    
-    RMSEall = zeros([11 11 3]);
-    ind2examine = 1:3;
-    blockNums = 2:7;
-end
 trialNumAll = 1:36;
 wLunq = [1 -1];
 wMunq = [1 -1];
@@ -140,7 +91,33 @@ for Sindex = 1:length(wSunq)
                 indStiml = abs(rgbAll(:,1)-rgbUnq(l,1))<0.001 & ...
                 abs(rgbAll(:,2)-rgbUnq(l,2))<0.001 & ...
                 abs(rgbAll(:,3)-rgbUnq(l,3))<0.001;
-                defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS10(wvInFocus(l),875);
+                if subjNum==10
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS10(wvInFocus(l),875);
+                end
+                if subjNum==1
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS1(wvInFocus(l),875);
+                end
+                if subjNum==3
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS3(wvInFocus(l),875);
+                end
+                if subjNum==5
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS5(wvInFocus(l),875);
+                end                
+                if subjNum==9
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS9(wvInFocus(l),875);
+                end
+                if subjNum==16
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS16(wvInFocus(l),875);
+                end
+                if subjNum==17
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS17(wvInFocus(l),875);
+                end
+                if subjNum==18
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS18(wvInFocus(l),875);
+                end                
+                if subjNum==20
+                    defocus875predTmp(indStiml) = optDistAll(indStiml)-humanWaveDefocusS20(wvInFocus(l),875);
+                end                                
                 wvInFocusTmp(indStiml) = wvInFocus(l);
             end
             RMSE(i,j,Sindex) = sqrt(mean((defocus875predTmp(:)-defocus875(:)).^2));
