c237862ea3bd3283a1e77544dc2572306a1975e3
Benjamin Chin <benjaminchin@wifi-10-40-6-184.wifi.berkeley.edu>
2025-04-15 13:30:23 -0700

    wavelength-in-focus with Strehl metric working (I think)

--- ARCwvInFocusMeanZstrehl.m
+++ ARCwvInFocusMeanZstrehl.m
@@ -1,5 +1,6 @@
 function wvInFocus = ARCwvInFocusMeanZstrehl(subjNum,rgb,wLMS)
 
+% DEFINING WAVELENGTH
 wave = 380:4:780;
 nFocus = length(wave);
 foldernameCones = '/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/CIVO_BVAMS/data/coneImages/';
@@ -29,18 +30,19 @@ if bUseBVAMScal
     load([calPath 'bluePrimaryJuly0624_initialPositionFocus3_100.mat']);
     d.spd(:,3) = energy;
 end
-d.gamma(:,1) = (d.gamma(:,1).^(1/2.2)).^2.5;
-d.gamma(:,2) = (d.gamma(:,2).^(1/2.2)).^2.7;
-d.gamma(:,3) = (d.gamma(:,3).^(1/2.2)).^2.3;
 
+% CALCULATE STIMULUS ENERGY SPECTRUM FOR INPUT RGB VALUES
 rgbGammaCorrected = rgb.^[2.5 2.7 2.3];
 stimSPD = d.spd(:,1).*rgbGammaCorrected(1)+d.spd(:,2).*rgbGammaCorrected(2)+d.spd(:,3).*rgbGammaCorrected(3);
 
-% COLOR MATCHING FUNCTIONS
-S = [380 4 101]; % weird convention used by Brainard lab for defining wavelengths
-load T_xyz1931; % load color matching functions
-T_sensorXYZ = 683*SplineCmf(S_xyz1931,T_xyz1931,S); % interpolate and scale
-stimLum = stimSpd.*T_sensorXYZ;
+% LOAD CONE QUANTAL EFFICIENCIES
+load('/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/CIVO_BVAMS/data/helperFiles/sQE.mat');
+% LOAD TRANSMITTANCE VALUES
+load('/Users/benjaminchin/Library/CloudStorage/GoogleDrive-bechin@berkeley.edu/Shared drives/CIVO_BVAMS/data/helperFiles/transmittance.mat');
+
+% CALCULATE ENERGY THAT ACTUALLY GETS THROUGH THE LENS
+stimSPD = reshape(stimSPD,[1 1 length(wave)]);
+stimSPDtransmitted = bsxfun(@times,stimSPD,transmittance);
 
 % % Put the image center in (1, 1) and take the transform.
 % imgFFT = fft2(fftshift(img));
@@ -50,13 +52,19 @@ stimLum = stimSpd.*T_sensorXYZ;
 
 strehlNorm = [];
 
-for i = 1:nFocus
-    fnameConeRsp = ['subj' num2str(subjNum) 'stimulus' num2str(stimNum) 'focusInd' num2str(i)];
-    load([foldernameCones 'S' num2str(subjNum) '/' fnameConeRsp]);
-    psfRC = reshape(psf,[size(psf,1)*size(psf,2) size(psf,3)]);
-    psfPolyLumRC = sum(bsxfun(@times,psfRC,stimLum),2);
-    psfPolyLumXY = reshape(psfPolyLumRC,[size(psf,1) size(psf,2) size(psf,3)]);
-    strehlNorm(i) = max(psfPolyLumXY(:));
+for i = 1:nFocus % FOR EACH WAVELENGTH
+    % LOAD POINT SPREAD FUNCTIONS
+    fnameConeRsp = ['subj' num2str(subjNum) 'stimulus12focusInd' num2str(i) 'psf'];
+    S = load([foldernameCones 'S' num2str(subjNum) '/' fnameConeRsp]);
+    % SCALE EACH POINT SPREAD FUNCTION BY ENERGY GETTING PAST LENS
+    psfIrradianceScaled = bsxfun(@times,S.psf,stimSPDtransmitted);
+    psfCone = [];
+    for j = 1:size(sQE,2) % FOR EACH CONE TYPE
+        sQEsingleCone = reshape(sQE(:,j),[1 1 length(wave)]);
+        psfCone(:,:,j) = sum(bsxfun(@times,psfIrradianceScaled,sQEsingleCone),3);
+    end
+    psfMech = wLMS(1).*psfCone(:,:,1) + wLMS(2).*psfCone(:,:,2) + wLMS(3).*psfCone(:,:,3);
+    strehlNorm(i) = max(psfMech(:));
 end
 
 [~,indPeakPeak] = max(strehlNorm);
